aea380c75d9369cd56865e9984bcead0955c5331
Jegadesh B <jegadesh213@gmail.com>
2022-11-11 22:07:27 +0530

    New Pull

---
+++ JegadeshB-main/web dev 1/index.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelectorAll('.hamburger');
+
+hamburger.addEventListener('click',function(){
+    this.classList.toggle('is-active');
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
+
+
+
